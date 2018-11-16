9947e75927f552ae7a403d62640c5320f04f9048
Yalin <yalin.gunayer@gmail.com>
2018-11-16 14:15:07 +0300

    :wrench: Promisify redis test

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-require('../redis');
+const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+
+const testRedis = require('./redis');
+
+const tests = [
+    testRedis(redisUrl)
+];
+
+Promise.all(tests)
+    .then(() => {
+        console.info('All tests were successful.');
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(-1);
+    })
--- redis.js
+++ redis.js
@@ -1,24 +1,29 @@
 const redis = require('redis');
 
-const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+async function test(redisUrl) {
+    return new Promise((resolve, reject) => {
+        console.log(`Connecting to redis at ${redisUrl}`);
 
-console.log(`Connecting to redis at ${redisUrl}`);
+        const client = redis.createClient({ url: redisUrl });
 
-const client = redis.createClient({ url: redisUrl });
+        client.on('connect', () => console.info('Connected to Redis'));
 
-client.on('connect', () => console.info('Connected to Redis'));
+        client.on('error', err => {
+            console.error('Failed to connect to Redis', err);
+            reject(err);
+        });
 
-client.on('error', err => {
-    console.error('Failed to connect to Redis', err);
-    process.exit(1);
-});
+        client.keys('*', (err, keys) => {
+            if (err) {
+                console.error('Failed to execute command', err);
+                reject(err);
+                return;
+            }
 
-client.keys('*', (err, keys) => {
-    if (err) {
-        console.error('Failed to execute command', err);
-        return process.exit(1);
-    }
+            console.info(`Successfully retrieved ${keys.length} keys from Redis`);
+            resolve();
+        });
+    });
+}
 
-    console.info(`Successfully retrieved ${keys.length} keys from Redis`);
-    client.quit(() => process.exit(0));
-});
+module.exports = test;
